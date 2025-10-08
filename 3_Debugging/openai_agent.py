2ea3523010fe621d6c451f924daa01bf55c43c26
anil-kumar1560 <anilgummadidala20@gmail.com>
2025-10-07 20:40:47 -0400

    Langraph debugging

---
+++ 3_Debugging/openai_agent.py
@@ -0,0 +1,41 @@
+from typing import Annotated
+from typing_extensions import TypedDict
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph,START, END
+from langgraph.graph.message import add_messages
+from langchain_core.messages import BaseMessage
+from langgraph.prebuilt import ToolNode
+from langchain_core.tools import tool
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+os.environ["LANGSMITH_API_KEY"]=os.getenv("Langchain_API_KEY")
+
+class State(TypedDict):
+    messages:Annotated[list[BaseMessage],add_messages]
+
+model=ChatOpenAI(temperature=0)
+
+
+def make_default_graph():
+    graph_workflow=StateGraph(State)
+
+    def call_model(state):
+        return {"message":[model.invoke(state['messages'])]}
+    
+    graph_workflow.add_node("agent",call_model)
+
+    graph_workflow.add_edge(START,"agent")
+    graph_workflow.add_edge("agent",END)
+
+    agent=graph_workflow.compile()
+
+    return agent
+
+agent=make_default_graph()
+
+
